014263fba2234d672547dd715153ee1843df22f6
student apnaproject <varishali886885@gmail.com>
2026-04-21 22:53:22 +0530

    added some code inside varish.py

--- varish.py
+++ varish.py
@@ -1,1 +1,13 @@
-print("hello world")
+count = 1
+while count <= 5 :
+     print("hello")
+     count = count + 1
+
+print(count)
+
+i = 1
+while i <= 10 :
+    print("varish ali", i)
+    i += 1
+print(i)
+
