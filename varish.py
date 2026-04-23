58b5825ea6ff3a8f4c03668057c07fc21087e619
student apnaproject <varishali886885@gmail.com>
2026-04-23 07:34:30 +0530

    added a new code in varish.py

--- varish.py
+++ varish.py
@@ -1,12 +1,3 @@
-
-nums = (1, 5, 7, 9, 21, 32, 54, 21)
-x = 21
-idx = 0
-for el in nums :
-
-    if(el == x):
-        print("number found at idx", idx)
-        break
-    idx += 1    
-
-    
+n = int(input("Enter a number:-"))
+for i in range(1,11):
+    print(n*i)
