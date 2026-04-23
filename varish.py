3ccdc6065fdb2dc586b161ecb8d040dff7c4f525
student apnaproject <varishali886885@gmail.com>
2026-04-23 12:06:04 +0530

    added a new code varish.py

--- varish.py
+++ varish.py
@@ -1,5 +1,6 @@
-while i <= n:
-    sum += i
-    i += 1
-print("total sum:-",sum)    
-  
+def calc_sum(n):
+    if(n == 0):
+        return 0
+    return calc_sum(n-1) + n
+sum = calc_sum(10)
+print(sum)
