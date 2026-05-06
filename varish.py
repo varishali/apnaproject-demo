9ebda678b30a5266ca6a1828aab5ab7b20bad114
student apnaproject <varishali886885@gmail.com>
2026-05-06 17:28:18 +0530

    calculator code added

--- varish.py
+++ varish.py
@@ -1,6 +1,43 @@
-def calc_sum(n):
-    if(n == 0):
-        return 0
-    return calc_sum(n-1) + n
-sum = calc_sum(10)
-print(sum)
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+print("Choose Operator : [+,-,*,/,**]")
+
+op = input("Enter Operator : ")
+
+if op == "+":
+    print("Addition is : ",num1+num2)
+
+elif op == "-":
+    print("Subtraction : ",num1-num2)
+
+elif op == "*":
+    print("Multiplication is : ",num1*num2)
+
+elif op == "/":
+    if num2 != 0:
+        print("Division is : ",num1/num2)
+    else:
+        print("Cannot Divide by Zero! ")
+
+
+elif op == "**":
+    print("Exponent is : ",num1**num2)
+
+else:
+    print("Invalid Operator!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
