6bd76573584b9ae75e3e08acdf9f9da0bc7be6d9
student apnaproject <varishali886885@gmail.com>
2026-05-08 12:47:08 +0530

    kon banega krorepati

--- varish.py
+++ varish.py
@@ -1,18 +1,67 @@
-import time
+# kon banega crorepati 2026b
 
-t = time.strftime('%H:%M:%S')
-hour = int(time.strftime('%H'))
-hour = int(input("Enter hour : "))
+print("===welcome to kon banega crorepati 2026===")
 
+money = 0
 
-if(hour>0 and hour<12):
-    print("Good Morning Sir!")
+#question 1
+print("Q1. What is tha capital od India?")
+print("a. Mumbai")
+print("b. dehli")
+print("c. Kolkata")
+print("d. Chennai")
 
-elif(hour>=12 and hour<=17):
-    print("Good After Noon Sir!")
+answer1 = input("Enter your answer:")
 
-elif(hour>=17 and hour<0):
-    print("Good Night Sir!")
+if answer1 == "b":
+    print("cprrect answer")
+    money += 1000
+    print("you have won :",money)
+
+    #question 2
+    print("Next Question for -- 5000 --")
+    print("Q2. what is the national animals of India ?")
+    print("a. Tiger")
+    print("b. Lion")
+    print("c. Elephant")
+    print("d. Bear")
+
+    answer2 = input("Enter your answer:")
+
+    if answer2 == "a":
+        print("correct answer")
+        money += 5000
+        print("you have won :",money)
+    
+
+        #question 3
+        print("Next question for -- 10000 --")
+        print("Q3. what is the national bords of India ?")
+        print("a. peacock")
+        print("b. piegon")
+        print("c. crow")
+        print("d. duck")
+
+        answer3 = input("Enter your answer:")
+        if answer3 =="a":
+            print("correct answer")
+            money += 10000
+            print("you have won :",money)
+
+        else:
+            print("wrong answer!")
+            print("game over!")  
+            print("money won :",money)  
+    
+    else:
+        print("wrong answer!")
+        print("game over !") 
+        print("money won :",money)
+else:
+    print("wrong answer!")
+    print("game over !")
+    print("money won :",money)
+
 
 
 
