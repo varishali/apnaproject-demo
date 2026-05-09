28efc86d19f3dcd9b76bd0dee9a0c467c9f308df
student apnaproject <varishali886885@gmail.com>
2026-05-09 15:06:56 +0530

    tic_tac_toe

--- varish.py
+++ varish.py
@@ -1,41 +1,83 @@
-balance = 5000
+# tic tak game :
+# 1. creat board
 
-print("welcome to ATTM ")
+board = [" "," "," ",
+         " "," "," ",
+         " "," "," "]
+
+current_player = "X"
+
+def print_board():
+    print()
+    print(board[0], "/", board[1], "/", board[2])
+    print(board[3], "/", board[4], "/", board[5])
+    print(board[6], "/", board[7], "/", board[8])
+    print()
+
+
+
+    #win condition
+wins = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 while True:
-    print("1. cheak balance")
-    print("2. deposite money")
-    print("3. withdraw money")
-    print("4. exit")
+    print_board()
 
-    choice = input("Enter choice(1-4):")
+    position = input(f"player {current_player}, choose position(1-9):")
+    
+    #cheak number only
+    if not position.isdigit():
+        print("please enter number only")
+        continue
+    position = int(position) - 1
+    #cheak range
+    if position < 0 or position >8:
+        print("choose between 1 to 9 only")
+        continue
 
-    if choice == "1":
-        print("your balance is :",balance)
+    #cheak empty position
+    
+    if board[position] != " ":
+        print("position already taken,try again")
+        continue
 
-    elif choice == "2":
-        amount = int(input("Enter diposite amount :"))
-        balance = balance + amount
-        print("money diposited succesfully")
-        print("new balance :",balance)
-        
+    board[position] = current_player
 
-    elif choice == "3":
-        amount = int(input("Enter withdraw amount:"))
-        if amount<=balance:
-            balance = balance - amount
-            print("please collect your cash:")
-            print("remainning balance :",balance)
+    winner = False
+    for condition in wins:
+        a,b,c = condition
 
-        else:
-            print("Insufficient balance")
+        if board[a] == board[b] == board[c] != " ":
+            print_board()
+            print(f"player {current_player} wins!")
 
-    elif choice == "4":
-        print("thankyou for using ATM")
+            winner = True
+            break
+    if winner:
         break
 
+    #match drawn
+    if " " not in board :
+        print_board()
+        print("match draw!")
+        break
+    #change player
+    if current_player == "X":
+        current_player = "O"
+
     else:
-        print("Invalid choice")            
+        current_player = "X"      
+    
+
+
 
 
 
