baa4a5b2ef00c33e17830d9ac1c5b09e87426f4b
student apnaproject <varishali886885@gmail.com>
2026-05-12 12:22:17 +0530

    Stylish Calculator

--- varish.py
+++ varish.py
@@ -1,10 +1,31 @@
-import math
-num = int(input("enter th enumber : "))
+print("================")
+print("   CALCULATOR   ")
+print("================")
 
-print("squre root : ",math.sqrt(num))
-print("power : ",math.pow(num,2))
-print("factorial : ",math.factorial(num))
+a = int(input("enter first number : "))
+b = int(input("enter second number : "))
 
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("enter your choice : ")
+
+if choice == "1":
+    print("Result : ",a+b)
+
+elif choice == "2":
+    print("Result : ",a-b)
+
+elif choice == "3":
+    print("Result : ",a*b)
+
+elif choice == "4":
+    #if b != 0 :
+    print("Result : ",a/b)
+    #else:
+       # print("Error : Division by zero is not allowed.")               
 
 
 
