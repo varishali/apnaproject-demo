fb4d9694db7d617d92be576feededd4b092e22ee
student apnaproject <varishali886885@gmail.com>
2026-05-19 16:44:42 +0530

    Dice Rolling Game PY

---
+++ dice_rolling.py
@@ -0,0 +1,22 @@
+# Dice Rolling Semulator
+
+import random
+print("\033[1;92m")
+print("=" *30)
+print("       DISE ROLLING GAME")
+print("=" *30)
+print("\033[0m")
+
+while True:
+    input("\033[1;93mPress Enter To Roll Dice...\033[0m")
+
+    dice = random.randint(1, 6)
+
+    print(f"\n\033[1;95mYou Rolled : {dice}\033[0m")
+
+    again = input("\033[1;93mRoll Again? (y/n) :\033[0m ").lower()
+
+    if again != "y":
+        print("\033[1;95mGame Over!\033[0m")
+
+    break    
