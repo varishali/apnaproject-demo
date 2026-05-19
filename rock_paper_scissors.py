087bd823168ca45164a8e104d3c74e64ba1f5f53
student apnaproject <varishali886885@gmail.com>
2026-05-19 16:41:09 +0530

    Dice Rolling Semulator By Usig PY

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,39 +1,22 @@
-import random
+# Dice Rolling Semulator
 
+import random
 print("\033[1;92m")
-print("=" * 34)
-print("     ROCK PAPER SCISSORS GAME")
-print("=" *34)
+print("=" *30)
+print("       DISE ROLLING GAME")
+print("=" *30)
 print("\033[0m")
 
-choices = ["rock","paper","scissors"]
-
 while True:
-    user = input("\033[1;93mEnter Rock, Paper, or Scissors : \033[0m").lower()
+    input("\033[1;93mPress Enter To Roll Dice...\033[0m")
 
-    computer = random.choice(choices)
+    dice = random.randint(1, 6)
 
-    print(f"\n\033[1;94mComputer Chose : {computer}\033[0m")
+    print(f"\n\033[1;95mYou Rolled : {dice}\033[0m")
 
-    if user == computer:
-        print("\033[1;94mIt's a Draw!\033[0m")
+    again = input("\033[1;93mRoll Again? (y/n) :\033[0m ").lower()
 
-    elif (
-        (user == "rock" and computer == "scissers")or
-        (user == "paper" and computer == "rock")or
-        (user == "scissers" and computer == "paper")
-    ):
-        print("\033[1;94mYou Win!\033[0m")
+    if again != "y":
+        print("\033[1;95mGame Over!\033[0m")
 
-    elif user in choices :
-        print("\033[1;91mComputer Wins!\033[0m")    
-
-    else:
-        print("\033[1;94mInvalid Input!\033[0m")
-
-    play_again = input("\033[1;94mPlay Again ? (yes/no) : \033[0m").lower()
-
-    if play_again != "yes":
-        print("\n\033[3;95mThanks For Playing.\033[0m ")
-
-        break       
+    break    
