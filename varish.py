08d1e8596cd9f343c4c83fb0cc1e72aefa3e257b
student apnaproject <varishali886885@gmail.com>
2026-05-22 09:12:08 +0530

    Data Frame panda

--- varish.py
+++ varish.py
@@ -1,72 +1,24 @@
-class Account:
-    print("----------------------")
-    print("\033[1mWELCOME TO VARISH BANK\033[0m")
-    print("----------------------")
-    def __init__(self,bal,acc,password):
-        self.balance = bal
-        self.account_number = acc
-        self.password = password
+import pandas as pd
+data = {
+    "Name" : ["varish","ali","sohil"],
+    "Marks" : [78,89,67]
+}
+df = pd.DataFrame(data)
 
-    def debit(self,amount):
-        if amount > self.balance:
-            print("Insufficient balance.")
+#add new column
+df["Result"] = ["pass","fail","pass"]
+print(df)
+print("\n")
+df["City"] = "Dehli"
 
-        else:    
-            self.balance -= amount
-            print("RS.",amount,"was debited.")
-            print("total balance is",self.get_balance())
+print(df)
+print("\n")
 
-    def credit(self,amount):
-        self.balance += amount
-        print("RS.",amount,"was credited.")
-        print("total balance is ",self.get_balance())
+# delete Coloumn
+df = df.drop("City",axis=1)
+print(df)   
 
-    def get_balance(self):
-        return self.balance
-        
-acc1 = Account(15000,1234567890,"12345")
+#rename colummn
+df = df.rename(columns={"Marks":"Score"})
+print(df)   
 
-password = input("enter your password to continue : ")
-
-
-if password == acc1.password:
-    print("--- \033[1mlogin succesful\033[0m ---")
-    print("current balance :",acc1.get_balance())
-
-
-    options = int(input("Enter (1) for credit and (2) for debit :"))
-
-    if options == 1:
-        print("you have selected credit option :")
-        input1 = int(input("Enter the amount to credit: ") )
-        acc1.credit(input1)
-
-    elif options == 2:     
-        print("you have selected debit option :")
-        input1 = int(input("enter the amount to debit: "))
-        acc1.debit(input1) 
-    else:
-        print("Invalid options!")      
-
-else:
-    print("Invalid password!")
-    print("Please try again")         
-
-
-
-
-
-
-
-
-
-
-      
-    
-
-
-
-
-   
-
-        
