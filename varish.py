3ad3ef206f7a4e5741869eb3578994b7329a6280
student apnaproject <varishali886885@gmail.com>
2026-05-26 09:20:50 +0530

    matrix calculator

--- varish.py
+++ varish.py
@@ -1,24 +1,13 @@
-import pandas as pd
-data = {
-    "Name" : ["varish","ali","sohil"],
-    "Marks" : [78,89,67]
-}
-df = pd.DataFrame(data)
-
-#add new column
-df["Result"] = ["pass","fail","pass"]
-print(df)
-print("\n")
-df["City"] = "Dehli"
-
-print(df)
-print("\n")
-
-# delete Coloumn
-df = df.drop("City",axis=1)
-print(df)   
-
-#rename colummn
-df = df.rename(columns={"Marks":"Score"})
-print(df)   
-
+import numpy as np
+a = np.array([
+    [1,2],
+    [3,4]
+])
+b = np.array([
+    [5,6],
+    [7,8]
+])
+print("Matrix Addition : ")
+print(a+b)
+print("\nMatrix Dot Product : ")
+print(np.dot(a,b))
