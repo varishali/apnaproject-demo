14e91e0fe9dda56ca14f1dd348b3c60491676591
student apnaproject <varishali886885@gmail.com>
2026-05-28 15:51:54 +0530

    EID MUBARAK

--- varish.py
+++ varish.py
@@ -1,12 +1,37 @@
-import seaborn as sns
 import matplotlib.pyplot as plt
-import pandas as pd
 
-data = {
-    "Marks" : [78,90,67,88],
-    "Attendance" : [90,95,70,85],
-    "Study" : [2,5,1,4]
-}
-df = pd.DataFrame(data)
-sns.pairplot(df)
+fig, ax = plt.subplots(figsize=(8,5))
+
+fig.patch.set_facecolor("black")
+
+ax.set_facecolor("black")
+
+# moon
+moon = plt.Circle(
+    (0.5,0.5),
+    (0.2),
+    color="yellow"
+)
+
+# cut part
+cut = plt.Circle(
+    (0.58,0.55),
+    0.2,
+    color="black"
+)
+ax.add_patch(moon)
+ax.add_patch(cut)
+
+# text
+plt.text(
+    0.2,
+    0.2,
+    "EID MUBARAK",
+    fontsize=28,
+    color="cyan",
+    fontweight="bold" 
+)
+
+plt.axis("off")
+
 plt.show()
