c9c1151134a5e943990251ce2f98fe97b25d062e
student apnaproject <varishali886885@gmail.com>
2026-05-28 16:02:42 +0530

    numpy

--- varish.py
+++ varish.py
@@ -1,9 +1,5 @@
-import pandas as pd
-data = {
-    "Name" : ["Varish","Zainul","Ali","Sohil"],
-    "Math_number" : [89,78,90,76,],
-    "Physics_number" : [78,98,92,45,],
-    "City" : ["Dehli","Mumbai","Lucknow","Dehli"]
-}
-df = pd.DataFrame(data)
-print(df)
+import numpy as np
+
+arr = np.array([78, 90, 67])
+
+print(np.min(arr))
