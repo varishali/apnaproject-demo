c9921b7009ce66d16f50e36f5f02e0b7ec138829
student apnaproject <varishali886885@gmail.com>
2026-05-28 15:59:37 +0530

    panda programe

--- varish.py
+++ varish.py
@@ -1,37 +1,9 @@
-import matplotlib.pyplot as plt
-
-fig, ax = plt.subplots(figsize=(8,5))
-
-fig.patch.set_facecolor("black")
-
-ax.set_facecolor("black")
-
-# moon
-moon = plt.Circle(
-    (0.5,0.5),
-    (0.2),
-    color="yellow"
-)
-
-# cut part
-cut = plt.Circle(
-    (0.58,0.55),
-    0.2,
-    color="black"
-)
-ax.add_patch(moon)
-ax.add_patch(cut)
-
-# text
-plt.text(
-    0.2,
-    0.2,
-    "EID MUBARAK",
-    fontsize=28,
-    color="cyan",
-    fontweight="bold" 
-)
-
-plt.axis("off")
-
-plt.show()
+import pandas as pd
+data = {
+    "Name" : ["Varish","Zainul","Ali","Sohil"],
+    "Math_number" : [89,78,90,76,],
+    "Physics_number" : [78,98,92,45,],
+    "City" : ["Dehli","Mumbai","Lucknow","Dehli"]
+}
+df = pd.DataFrame(data)
+print(df)
