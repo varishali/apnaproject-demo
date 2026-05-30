7a38b0bbcabdcfc7afe373347650f0d9f3813a8b
student apnaproject <varishali886885@gmail.com>
2026-05-30 20:57:44 +0530

    Dice rolling game

--- varish.py
+++ varish.py
@@ -1,13 +1,20 @@
 import random
 
+score = 0
+
 while True:
 
     roll = input("Roll the dice? (y/n): ")
 
-    if roll == "y":
+    if roll.lower() == "y":
+
         number = random.randint(1, 6)
         print("You got:", number)
 
+        score += number
+        print("Current Score:", score)
+
     else:
-        print("Game Ended")
+        print("\nGame Ended")
+        print("Final Score:", score)
         break
