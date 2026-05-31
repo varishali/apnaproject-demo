12f324c3924165709ebf20201ff2c6ca32858146
student apnaproject <varishali886885@gmail.com>
2026-05-31 10:08:41 +0530

    countdown timer

--- varish.py
+++ varish.py
@@ -1,51 +1,14 @@
-class Patient:
-    def __init__(self, pid, name, age, disease):
-        self.pid = pid
-        self.name = name
-        self.age = age
-        self.disease = disease
+import time
 
-patients = []
-appointments = []
+seconds = int(input("Enter time in seconds: "))
 
-def add_patient():
-    pass
+while seconds > 0:
+    mins = seconds // 60
+    secs = seconds % 60
 
-def view_patients():
-    pass
+    print(f"{mins:02}:{secs:02}", end="\r")
 
-def search_patient():
-    pass
+    time.sleep(1)
+    seconds -= 1
 
-def book_appointment():
-    pass
-
-def generate_bill():
-    pass
-
-while True:
-    print("\n===== Hospital Management System =====")
-    print("1. Add Patient")
-    print("2. View Patients")
-    print("3. Search Patient")
-    print("4. Book Appointment")
-    print("5. View Appointments")
-    print("6. Generate Bill")
-    print("7. Exit")
-
-    choice = input("Enter choice: ")
-
-    if choice == "1":
-        add_patient()
-    elif choice == "2":
-        view_patients()
-    elif choice == "3":
-        search_patient()
-    elif choice == "4":
-        book_appointment()
-    elif choice == "5":
-        view_appointments()
-    elif choice == "6":
-        generate_bill()
-    elif choice == "7":
-        break
+print("\nTime's Up!")
