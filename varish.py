3c08e37c03120b8c9ca74d9e1f45ebc6a6a0040c
student apnaproject <varishali886885@gmail.com>
2026-05-31 10:03:16 +0530

    python game

--- varish.py
+++ varish.py
@@ -10,9 +10,9 @@ for question, answer in questions.items():
     user_answer = input(question).lower()
 
     if user_answer == answer:
-        print("Correct ✅")
+        print("Correct ")
         score += 1
     else:
-        print("Wrong ❌")
+        print("Wrong ")
 
 print("\nFinal Score:", score, "/", len(questions))
