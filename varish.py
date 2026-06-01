f73db1a6b3a8e1efe2bd267b38dcf99fc95b461f
student apnaproject <varishali886885@gmail.com>
2026-06-01 19:15:50 +0530

    Hnagman Game

--- varish.py
+++ varish.py
@@ -1,5 +1,30 @@
-num = int(input("Enter Number: "))
+import random
 
-print("Binary:", bin(num))
-print("Octal:", oct(num))
-print("Hex:", hex(num))
+words = ["python", "apple", "computer", "coding", "school"]
+
+word = random.choice(words)
+guessed = ["_"] * len(word)
+
+lives = 6
+
+while lives > 0 and "_" in guessed:
+
+    print("\nWord:", " ".join(guessed))
+    print("Lives:", lives)
+
+    letter = input("Enter a letter: ").lower()
+
+    if letter in word:
+        for i in range(len(word)):
+            if word[i] == letter:
+                guessed[i] = letter
+    else:
+        lives -= 1
+        print("Wrong Guess!")
+
+if "_" not in guessed:
+    print("\nYou Win!")
+    print("Word was:", word)
+else:
+    print("\nGame Over!")
+    print("Word was:", word)
