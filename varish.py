7d9c7f0aa0f3374adc38c9a0487abf1b23671f15
student apnaproject <varishali886885@gmail.com>
2026-06-01 19:17:49 +0530

    Mad Libs Game

--- varish.py
+++ varish.py
@@ -1,30 +1,5 @@
-import random
+name = input("Name: ")
+place = input("Place: ")
+animal = input("Animal: ")
 
-words = ["python", "apple", "computer", "coding", "school"]
-
-word = random.choice(words)
-guessed = ["_"] * len(word)
-
-lives = 6
-
-while lives > 0 and "_" in guessed:
-
-    print("\nWord:", " ".join(guessed))
-    print("Lives:", lives)
-
-    letter = input("Enter a letter: ").lower()
-
-    if letter in word:
-        for i in range(len(word)):
-            if word[i] == letter:
-                guessed[i] = letter
-    else:
-        lives -= 1
-        print("Wrong Guess!")
-
-if "_" not in guessed:
-    print("\nYou Win!")
-    print("Word was:", word)
-else:
-    print("\nGame Over!")
-    print("Word was:", word)
+print(f"{name} went to {place} and saw a {animal}.")
