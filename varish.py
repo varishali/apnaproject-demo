39a4c59f557dc317d4bcf4974b787039a70554a2
student apnaproject <varishali886885@gmail.com>
2026-06-03 20:40:18 +0530

    Student Result Analyzer

--- varish.py
+++ varish.py
@@ -1,14 +1,30 @@
-import random
+students = {
+    "Ali": 78,
+    "Rahul": 92,
+    "Sahil": 65,
+    "Aman": 85
+}
 
-user = random.randint(1, 6)
-computer = random.randint(1, 6)
+average = sum(students.values()) / len(students)
 
-print("You:", user)
-print("Computer:", computer)
+highest = max(students, key=students.get)
+lowest = min(students, key=students.get)
 
-if user > computer:
-    print("You Win!")
-elif computer > user:
-    print("Computer Wins!")
-else:
-    print("Draw!")
+print("Average Marks:", average)
+print("Topper:", highest, students[highest])
+print("Lowest:", lowest, students[lowest])
+
+print("\nGrades")
+
+for name, marks in students.items():
+
+    if marks >= 90:
+        grade = "A"
+    elif marks >= 75:
+        grade = "B"
+    elif marks >= 60:
+        grade = "C"
+    else:
+        grade = "D"
+
+    print(name, "-", grade)
