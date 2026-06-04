31e7433eb5363ed00b126a368cacae555334d1dc
student apnaproject <varishali886885@gmail.com>
2026-06-04 21:37:26 +0530

    Voiting System Project

--- varish.py
+++ varish.py
@@ -1,13 +1,51 @@
-import time
+votes = {
+    "Aman": 0,
+    "Rahul": 0,
+    "Sahil": 0
+}
 
-sentence = "Python is a powerful language"
+while True:
 
-print(sentence)
+    print("\n===== Voting System =====")
+    print("1. Cast Vote")
+    print("2. View Results")
+    print("3. Declare Winner")
+    print("4. Exit")
 
-start = time.time()
+    choice = input("Enter Choice: ")
 
-typed = input("Type Here: ")
+    if choice == "1":
 
-end = time.time()
+        print("\nCandidates:")
+        for candidate in votes:
+            print(candidate)
 
-print("Time Taken:", round(end - start, 2), "seconds")
+        candidate = input("\nEnter Candidate Name: ")
+
+        if candidate in votes:
+            votes[candidate] += 1
+            print("Vote Cast Successfully!")
+        else:
+            print("Invalid Candidate!")
+
+    elif choice == "2":
+
+        print("\n===== Results =====")
+
+        for candidate, vote_count in votes.items():
+            print(f"{candidate} : {vote_count} Votes")
+
+    elif choice == "3":
+
+        winner = max(votes, key=votes.get)
+
+        print("\n===== Winner =====")
+        print(f"{winner} won with {votes[winner]} votes!")
+
+    elif choice == "4":
+
+        print("Thanks For Using Voting System!")
+        break
+
+    else:
+        print("Invalid Choice!")
