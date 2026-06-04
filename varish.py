ee279d39d4e2a920895068e7198d67332f7363c7
student apnaproject <varishali886885@gmail.com>
2026-06-04 21:32:12 +0530

    student report card system

--- varish.py
+++ varish.py
@@ -1,57 +1,56 @@
-tasks = []
+students = {}
 
 while True:
 
-    print("\n===== TO-DO LIST =====")
-    print("1. Add Task")
-    print("2. View Tasks")
-    print("3. Remove Task")
-    print("4. Exit")
+    print("\n===== Student Report Card =====")
+    print("1. Add Student")
+    print("2. View Report")
+    print("3. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
 
-        task = input("Enter Task: ")
-        tasks.append(task)
+        name = input("Enter Student Name: ")
 
-        print("Task Added Successfully!")
+        marks = []
+
+        for i in range(3):
+            mark = float(input(f"Enter Subject {i+1} Marks: "))
+            marks.append(mark)
+
+        students[name] = marks
+
+        print("Student Added Successfully!")
 
     elif choice == "2":
 
-        if len(tasks) == 0:
-            print("No Tasks Found!")
+        if len(students) == 0:
+            print("No Students Found!")
 
         else:
-            print("\nTasks:")
 
-            for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
+            for name, marks in students.items():
+
+                average = sum(marks) / len(marks)
+
+                if average >= 90:
+                    grade = "A"
+                elif average >= 75:
+                    grade = "B"
+                elif average >= 60:
+                    grade = "C"
+                else:
+                    grade = "D"
+
+                print("\nName:", name)
+                print("Marks:", marks)
+                print("Average:", round(average, 2))
+                print("Grade:", grade)
 
     elif choice == "3":
-
-        if len(tasks) == 0:
-            print("No Tasks To Remove!")
-
-        else:
-
-            for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
-
-            try:
-                index = int(input("Enter Task Number: "))
-                removed_task = tasks.pop(index - 1)
-
-                print(f"{removed_task} Removed Successfully!")
-
-            except:
-                print("Invalid Task Number!")
-
-    elif choice == "4":
-
-        print("Thanks For Using To-Do List!")
+        print("Thank You!")
         break
 
     else:
-
         print("Invalid Choice!")
