bfc27f725988604de008a5046d62204c7c5b8527
student apnaproject <varishali886885@gmail.com>
2026-06-05 14:02:52 +0530

    Password

--- varish.py
+++ varish.py
@@ -1,51 +1,11 @@
-votes = {
-    "Aman": 0,
-    "Rahul": 0,
-    "Sahil": 0
-}
+import random
+import string
 
-while True:
+password = "".join(
+    random.choices(
+        string.ascii_letters + string.digits,
+        k=8
+    )
+)
 
-    print("\n===== Voting System =====")
-    print("1. Cast Vote")
-    print("2. View Results")
-    print("3. Declare Winner")
-    print("4. Exit")
-
-    choice = input("Enter Choice: ")
-
-    if choice == "1":
-
-        print("\nCandidates:")
-        for candidate in votes:
-            print(candidate)
-
-        candidate = input("\nEnter Candidate Name: ")
-
-        if candidate in votes:
-            votes[candidate] += 1
-            print("Vote Cast Successfully!")
-        else:
-            print("Invalid Candidate!")
-
-    elif choice == "2":
-
-        print("\n===== Results =====")
-
-        for candidate, vote_count in votes.items():
-            print(f"{candidate} : {vote_count} Votes")
-
-    elif choice == "3":
-
-        winner = max(votes, key=votes.get)
-
-        print("\n===== Winner =====")
-        print(f"{winner} won with {votes[winner]} votes!")
-
-    elif choice == "4":
-
-        print("Thanks For Using Voting System!")
-        break
-
-    else:
-        print("Invalid Choice!")
+print(password)
