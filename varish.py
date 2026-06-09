819ffd33f6243c4b4f636585d6a35eb9f64a1107
student apnaproject <varishali886885@gmail.com>
2026-06-09 17:27:51 +0530

    matplotlib graph

--- varish.py
+++ varish.py
@@ -1,8 +1,32 @@
-name = input("Enter Your Name: ")
+import matplotlib.pyplot as plt
+import pandas as pd
+names = ["a","b","c","d","e"]
+marks = [20,40,30,55,70]
 
-print("\nSuggested Usernames:")
-print(name + "123")
-print(name + "_official")
-print("its_" + name)
-print(name + "_gaming")
-print("mr_" + name)
+df = pd.DataFrame({"names":names,"marks":marks})
+
+plt.figure(facecolor="lightblue",figsize=(8,5))
+plt.gca().set_facecolor("lightyellow")
+
+plt.plot(df["names"],df["marks"],marker="o",color="lightcoral")
+
+plt.grid(linestyle="--",color="gray")
+plt.title(
+    "Marks of students",
+    color="red",
+    fontweight="bold",
+    fontstyle="italic",
+    bbox=dict(facecolor="lightgreen",boxstyle="round")
+    )
+plt.xlabel("Names",
+           color="red",
+           fontstyle="italic",
+           fontweight="bold",
+           bbox=dict(facecolor="lightgreen",boxstyle="round")
+           )
+plt.ylabel("Marks"
+           ,color="red",
+           fontstyle="italic",
+           fontweight="bold",
+           bbox=dict(facecolor="lightgreen",boxstyle="round"))
+plt.show()
