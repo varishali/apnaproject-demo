1f8cf8801022e27fdd4e440be6f41f14e7e5db54
student apnaproject <varishali886885@gmail.com>
2026-06-10 21:24:06 +0530

    OTP generator

--- varish.py
+++ varish.py
@@ -1,46 +1,20 @@
 import random
-import string
-
-urls = {}
 
 while True:
 
-    print("\n===== URL Shortener =====")
-    print("1. Shorten URL")
-    print("2. View URLs")
-    print("3. Exit")
+    print("\n===== OTP Generator =====")
+    print("1. Generate OTP")
+    print("2. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
 
-        long_url = input("Enter Long URL: ")
+        otp = random.randint(100000, 999999)
 
-        short_code = ''.join(
-            random.choices(
-                string.ascii_letters + string.digits,
-                k=6
-            )
-        )
-
-        urls[short_code] = long_url
-
-        print("\nShort URL:")
-        print(f"https://short.ly/{short_code}")
+        print("\nYour OTP:", otp)
 
     elif choice == "2":
-
-        if len(urls) == 0:
-            print("No URLs Found!")
-
-        else:
-
-            print("\nSaved URLs:")
-
-            for code, url in urls.items():
-                print(f"{code} -> {url}")
-
-    elif choice == "3":
         break
 
     else:
