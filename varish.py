dd37cc7b7bb8e3a26a0c8d9b05602938e6d86fc1
student apnaproject <varishali886885@gmail.com>
2026-06-12 22:23:16 +0530

    inventory management system

--- varish.py
+++ varish.py
@@ -1,52 +1,69 @@
-students = {}
+inventory = {}
 
 while True:
 
-    print("\n===== Attendance System =====")
-    print("1. Add Student")
-    print("2. Mark Attendance")
-    print("3. View Attendance")
-    print("4. Exit")
+    print("\n===== Inventory Management System =====")
+    print("1. Add Product")
+    print("2. View Products")
+    print("3. Update Stock")
+    print("4. Delete Product")
+    print("5. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
 
-        name = input("Enter Student Name: ")
+        product = input("Enter Product Name: ")
+        stock = int(input("Enter Stock Quantity: "))
 
-        students[name] = "Absent"
+        inventory[product] = stock
 
-        print("Student Added!")
+        print("Product Added Successfully!")
 
     elif choice == "2":
 
-        name = input("Enter Student Name: ")
-
-        if name in students:
-
-            status = input("Present/Absent: ")
-
-            students[name] = status
-
-            print("Attendance Updated!")
+        if len(inventory) == 0:
+            print("No Products Found!")
 
         else:
-            print("Student Not Found!")
+
+            print("\n===== Product List =====")
+
+            for product, stock in inventory.items():
+                print(f"{product} : {stock}")
 
     elif choice == "3":
 
-        if len(students) == 0:
-            print("No Students Found!")
+        product = input("Enter Product Name: ")
+
+        if product in inventory:
+
+            stock = int(input("Enter New Stock: "))
+            inventory[product] = stock
+
+            print("Stock Updated!")
 
         else:
-
-            print("\nAttendance Report")
-
-            for name, status in students.items():
-                print(f"{name} : {status}")
+            print("Product Not Found!")
 
     elif choice == "4":
+
+        product = input("Enter Product Name: ")
+
+        if product in inventory:
+
+            del inventory[product]
+
+            print("Product Deleted!")
+
+        else:
+            print("Product Not Found!")
+
+    elif choice == "5":
+
+        print("Thanks For Using Inventory System!")
         break
 
     else:
+
         print("Invalid Choice!")
