aed46ec88660744167a4571bf1169b829c9cee4c
student apnaproject <varishali886885@gmail.com>
2026-06-12 22:21:39 +0530

    Creat student attendence system

--- varish.py
+++ varish.py
@@ -1,51 +1,49 @@
-notes = []
+students = {}
 
 while True:
 
-    print("\n===== Notes Manager =====")
-    print("1. Add Note")
-    print("2. View Notes")
-    print("3. Delete Note")
+    print("\n===== Attendance System =====")
+    print("1. Add Student")
+    print("2. Mark Attendance")
+    print("3. View Attendance")
     print("4. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
 
-        note = input("Enter Note: ")
-        notes.append(note)
+        name = input("Enter Student Name: ")
 
-        print("Note Added Successfully!")
+        students[name] = "Absent"
+
+        print("Student Added!")
 
     elif choice == "2":
 
-        if len(notes) == 0:
-            print("No Notes Found!")
+        name = input("Enter Student Name: ")
+
+        if name in students:
+
+            status = input("Present/Absent: ")
+
+            students[name] = status
+
+            print("Attendance Updated!")
 
         else:
-
-            print("\nYour Notes:")
-
-            for i, note in enumerate(notes, start=1):
-                print(f"{i}. {note}")
+            print("Student Not Found!")
 
     elif choice == "3":
 
-        if len(notes) == 0:
-            print("No Notes To Delete!")
+        if len(students) == 0:
+            print("No Students Found!")
 
         else:
 
-            for i, note in enumerate(notes, start=1):
-                print(f"{i}. {note}")
+            print("\nAttendance Report")
 
-            num = int(input("Enter Note Number: "))
-
-            if 1 <= num <= len(notes):
-                deleted = notes.pop(num - 1)
-                print(f"{deleted} Deleted!")
-            else:
-                print("Invalid Note Number!")
+            for name, status in students.items():
+                print(f"{name} : {status}")
 
     elif choice == "4":
         break
