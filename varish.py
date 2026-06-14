6e66b55688a82db0984754ab1717087c1f738f5f
student apnaproject <varishali886885@gmail.com>
2026-06-14 22:36:13 +0530

    Creat Coupon Generator

--- varish.py
+++ varish.py
@@ -1,15 +1,11 @@
 import random
+import string
 
-otp = random.randint(1000,9999)
+coupon = ''.join(
+    random.choices(
+        string.ascii_uppercase + string.digits,
+        k=8
+    )
+)
 
-print("OTP : ",otp)
-
-user_otp = int(input("Enter OTP : "))
-
-if user_otp == otp:
-    print("Verified..")
-
-else:
-    print("Wrong OTP..")    
-
-
+print("Coupon Code : ",coupon)
