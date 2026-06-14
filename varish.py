cf0c138e0ff4e6834d936dac3be5c4ebd2ab6309
student apnaproject <varishali886885@gmail.com>
2026-06-14 22:20:20 +0530

    Creat Login System

--- varish.py
+++ varish.py
@@ -1,95 +1,18 @@
-class Room:
+username = "varish"
+password = "1234"
 
-    def __init__(self, room_no):
-        self.room_no = room_no
-        self.booked = False
+user = input("Enter username : ")
 
+if user == "varish":
 
-class Hotel:
+    pwd = input("Enter Pssword : ")
 
-    def __init__(self):
-        self.rooms = []
-
-        for i in range(101, 106):
-            self.rooms.append(Room(i))
-
-    def view_rooms(self):
-
-        print("\nRoom Status:")
-
-        for room in self.rooms:
-
-            status = "Booked" if room.booked else "Available"
-
-            print(f"Room {room.room_no} - {status}")
-
-    def book_room(self, room_no):
-
-        for room in self.rooms:
-
-            if room.room_no == room_no:
-
-                if not room.booked:
-
-                    room.booked = True
-                    print("Room Booked Successfully!")
-
-                else:
-                    print("Room Already Booked!")
-
-                return
-
-        print("Room Not Found!")
-
-    def cancel_booking(self, room_no):
-
-        for room in self.rooms:
-
-            if room.room_no == room_no:
-
-                if room.booked:
-
-                    room.booked = False
-                    print("Booking Cancelled!")
-
-                else:
-                    print("Room Already Available!")
-
-                return
-
-        print("Room Not Found!")
-
-
-hotel = Hotel()
-
-while True:
-
-    print("\n===== HOTEL BOOKING SYSTEM =====")
-    print("1. View Rooms")
-    print("2. Book Room")
-    print("3. Cancel Booking")
-    print("4. Exit")
-
-    choice = input("Enter Choice: ")
-
-    if choice == "1":
-
-        hotel.view_rooms()
-
-    elif choice == "2":
-
-        room_no = int(input("Enter Room Number: "))
-        hotel.book_room(room_no)
-
-    elif choice == "3":
-
-        room_no = int(input("Enter Room Number: "))
-        hotel.cancel_booking(room_no)
-
-    elif choice == "4":
-
-        break
+    if password == "1234":
+        print("Login Successful....")
 
     else:
+        print("Wrong Password...")
 
-        print("Invalid Choice!")
+else:
+    print("Invalid Username...")            
+
