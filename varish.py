e89f348b1d77bf4c3ce9c39f8fd21f2726cfd5d4
student apnaproject <varishali886885@gmail.com>
2026-06-15 21:58:00 +0530

    Number to word

--- varish.py
+++ varish.py
@@ -1,2 +1,16 @@
-for i in range(1,6):
-    print(" " * (5-i) + "* " * i)
+num = int(input("Enter Number (0-9): "))
+
+words = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+}
+
+print(words.get(num, "Invalid Number"))
