8bb4804725c984b0880a19e09ef85f1fed1e7fdd
student apnaproject <varishali886885@gmail.com>
2026-06-15 21:47:35 +0530

    Creat train ticket

--- varish.py
+++ varish.py
@@ -1,48 +1,83 @@
-balance = 10000
+tickets = []
+
+total_seats = 5
 
 while True:
 
-    print("\n===== ATM =====")
-    print("1. Check Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
+    print("\n===== TRAIN TICKET BOOKING SYSTEM =====")
+    print("1. Book Ticket")
+    print("2. View Passengers")
+    print("3. Cancel Ticket")
+    print("4. Available Seats")
+    print("5. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
 
-        print("Current Balance:", balance)
+        if len(tickets) >= total_seats:
+            print("No Seats Available!")
+
+        else:
+
+            name = input("Enter Passenger Name: ")
+            age = int(input("Enter Age: "))
+
+            seat_no = len(tickets) + 1
+
+            tickets.append({
+                "name": name,
+                "age": age,
+                "seat": seat_no
+            })
+
+            print(f"Ticket Booked Successfully!")
+            print(f"Seat Number: {seat_no}")
 
     elif choice == "2":
 
-        amount = float(input("Enter Amount: "))
-
-        if amount > 0:
-
-            balance += amount
-
-            print("Deposit Successful!")
+        if len(tickets) == 0:
+            print("No Bookings Found!")
 
         else:
 
-            print("Invalid Amount!")
+            print("\n===== PASSENGER LIST =====")
+
+            for passenger in tickets:
+
+                print(
+                    f"Name: {passenger['name']} | "
+                    f"Age: {passenger['age']} | "
+                    f"Seat: {passenger['seat']}"
+                )
 
     elif choice == "3":
 
-        amount = float(input("Enter Amount: "))
+        name = input("Enter Passenger Name: ")
 
-        if amount <= balance:
+        found = False
 
-            balance -= amount
+        for passenger in tickets:
 
-            print("Withdrawal Successful!")
+            if passenger["name"].lower() == name.lower():
 
-        else:
+                tickets.remove(passenger)
 
-            print("Insufficient Balance!")
+                found = True
+
+                print("Ticket Cancelled Successfully!")
+                break
+
+        if not found:
+            print("Passenger Not Found!")
 
     elif choice == "4":
+
+        available = total_seats - len(tickets)
+
+        print("Available Seats:", available)
+
+    elif choice == "5":
 
         print("Thank You!")
         break
