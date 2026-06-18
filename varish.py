291221e10e72d9addbceaed414bef10031aa9778
student apnaproject <varishali886885@gmail.com>
2026-06-18 10:12:51 +0530

    Voting System Program

--- varish.py
+++ varish.py
@@ -1,9 +1,56 @@
-number = input("Enter Mobile Number: ")
+votes = {
+    "Ali": 0,
+    "Ram": 0,
+    "Shyam": 0
+}
 
-if len(number) == 10 and number.isdigit():
+voted_users = []
 
-    print("Valid Number")
+while True:
 
-else:
+    print("\n1. Vote")
+    print("2. Result")
+    print("3. Exit")
 
-    print("Invalid Number")
+    choice = input("Enter Choice : ")
+
+    if choice == "1":
+
+        user = input("Enter Your Name : ")
+
+        if user in voted_users:
+            print("You Already Voted")
+
+        else:
+
+            print("\nCandidates")
+            print("Ali")
+            print("Ram")
+            print("Shyam")
+
+            candidate = input("Vote For : ")
+
+            if candidate in votes:
+
+                votes[candidate] += 1
+
+                voted_users.append(user)
+
+                print("Vote Added")
+
+            else:
+                print("Invalid Candidate")
+
+    elif choice == "2":
+
+        print("\nVoting Result")
+
+        for candidate, total_votes in votes.items():
+            print(candidate, ":", total_votes)
+
+    elif choice == "3":
+        print("Voting Closed")
+        break
+
+    else:
+        print("Invalid Choice")
