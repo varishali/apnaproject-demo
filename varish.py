d941013334f3cbb1567fe1cc09ca82c2209a7e9c
student apnaproject <varishali886885@gmail.com>
2026-06-20 21:45:50 +0530

    Build advanced login system

--- varish.py
+++ varish.py
@@ -1,17 +1,49 @@
-users = {
-    "varish" : "1234",
-    "ali" : "pass123"
-}
+users = {}
 
-print("=== LOGIN SYSTEM ===")
+while True :
+    print("\n\033[1;92m==== LOGIN SYSTEM ====\033[0m")
+    print("1. Signup")
+    print("2. Login")
+    print("3. Exit")
 
-username = input("Enter Username : ")
-password = input("Enter Password : ")
+    choice = input("Enter your Choice : ")
 
-if username in users:
-    if users[username] == password:
-        print("Login Successful!")
+    #signup
+    if choice == "1":
+        username = input("Creat username : ")
+        if username in users:
+            print("Username Already Exists! ")
+
+        else:
+            password = input("Creat password : ")
+            users[username] = password
+            print("Account Created Successfully!")
+
+    elif choice == "2":
+        username = input("Enter Username : ")
+        if username in users:
+
+            attempts = 3
+
+            while attempts > 0 :
+                password = input("Enter Password : ")
+
+                if users[username] == password:
+                    print("login Successfully!")
+                    break
+                else:
+                    attempts -= 1
+                    print("Wrong Password!")
+                    print("Attempts Left : ",attempts)
+
+            if attempts == 0:
+                print("Account Temporaily Locked!")
+                break
+           
+
+    elif choice == "3":
+        print("Program Closed!")
+        break
+
     else:
-        print("Wrong Password!")
-else:
-    print("Username Not Found!")            
+        print("Invalid Choice!")                        
