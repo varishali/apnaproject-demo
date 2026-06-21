6de46a60c124dddab37805019621bc10bf6e1ad9
student apnaproject <varishali886885@gmail.com>
2026-06-21 17:43:20 +0530

    Student Topper Founder

--- varish.py
+++ varish.py
@@ -1,24 +1,8 @@
-users = []
+students = {
+    "ali" : 90,
+    "varish" : 95,
+    "aman" : 80
+}
 
-while True:
-    print("\n1. Creat Username")
-    print("2. Exit")
-
-    choice = input("Enter Choice : ")
-
-    if choice == "1":
-        username = input("Create Username : ")
-        if username in users:
-            print("Username Alreaady Exists..")
-        
-        else:
-            users.append(username)
-            print("Account Created")
-        
-            print(users)
-  
-    elif choice == "2":
-        print("Program Closed...")
-        break
-    else:
-        print("Invalid Choice!")
+topper = max(students,key=students.get)
+print("Topper : ",topper)
