5d232b348aa5161fbf2f2d9c5bf3bce8a9725879
student apnaproject <varishali886885@gmail.com>
2026-06-23 15:12:24 +0530

    Shopping Cart System

--- varish.py
+++ varish.py
@@ -1,2 +1,43 @@
-text = input("Enter Text : ")
-print(text.replace(" ",""))
+cart = []
+
+total = 0
+
+while True:
+
+    print("\n1. Add Item")
+    print("2. View Cart")
+    print("3. Exit")
+
+    choice = input("Enter Choice: ")
+
+    if choice == "1":
+
+        item = input("Enter Item Name: ")
+
+        price = int(input("Enter Price: "))
+
+        cart.append((item, price))
+
+        total += price
+
+        print("Item Added!")
+
+    elif choice == "2":
+
+        print("\n===== CART =====")
+
+        for item, price in cart:
+
+            print(item, "-", price)
+
+        print("Total Bill:", total)
+
+    elif choice == "3":
+
+        print("Thank You!")
+
+        break
+
+    else:
+
+        print("Invalid Choice!")
