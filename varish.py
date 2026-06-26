dc0b1e852d18358a68dc793864514bbb3ec4ff91
student apnaproject <varishali886885@gmail.com>
2026-06-26 22:52:33 +0530

    Practice

--- varish.py
+++ varish.py
@@ -1,11 +1,10 @@
 import time
-
-for i in range(10, 0, -1):
-
-    print(i)
-
-    time.sleep(1)
-
-
-print("Time Up!")
-
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} took {time.perf_counter() - start:.4f}s")
+        return result
+    return wrapper
+@timer
+def compute(): return sum(i * i for i in range(10**6))
