9a048fdf0319f0c32b6d1034f67cfdba62e60325
student apnaproject <varishali886885@gmail.com>
2026-06-27 22:50:07 +0530

    Count Vowels

--- varish.py
+++ varish.py
@@ -1,108 +1,13 @@
-students = {}
+text = input("Enter Text : ")
+
+count = 0
 
 
-# ADD STUDENT
-def add_student():
+for ch in text.lower():
 
-    name = input("Enter Student Name : ")
+    if ch in "aeiou":
 
-    marks = int(input("Enter Marks : "))
-
-    students[name] = marks
-
-    print("Student Added Successfully!")
+        count += 1
 
 
-# VIEW RESULT
-def view_result():
-
-    if len(students) == 0:
-
-        print("No Student Found!")
-
-    else:
-
-        print("\n===== STUDENT RESULTS =====")
-
-        for name, marks in students.items():
-
-            print("\nName :", name)
-
-            print("Marks :", marks)
-
-
-            if marks >= 90:
-
-                print("Grade : A")
-
-            elif marks >= 70:
-
-                print("Grade : B")
-
-            elif marks >= 50:
-
-                print("Grade : C")
-
-            else:
-
-                print("Fail")
-
-
-# TOPPER
-def topper():
-
-    if len(students) == 0:
-
-        print("No Student Found!")
-
-    else:
-
-        top_student = max(students, key=students.get)
-
-        print("Topper :", top_student)
-
-        print("Marks :", students[top_student])
-
-
-# MAIN PROGRAM
-while True:
-
-    print("\n===== STUDENT RESULT SYSTEM =====")
-
-    print("1. Add Student")
-
-    print("2. View Results")
-
-    print("3. Show Topper")
-
-    print("4. Exit")
-
-
-    choice = input("Enter Choice : ")
-
-
-    if choice == "1":
-
-        add_student()
-
-
-    elif choice == "2":
-
-        view_result()
-
-
-    elif choice == "3":
-
-        topper()
-
-
-    elif choice == "4":
-
-        print("Program Closed!")
-
-        break
-
-
-    else:
-
-        print("Invalid Choice!")
+print("Vowels :", count)
