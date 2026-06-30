9db4ed5af4f00c6b62eab38de0b1033efcbd75b5
student apnaproject <varishali886885@gmail.com>
2026-06-30 23:14:02 +0530

    Build automated temp file cleaner

--- varish.py
+++ varish.py
@@ -1,25 +1,67 @@
+# File handling
+import os
+
+# Automatic scheduling
 import schedule
+
+# Time delay
 import time
+
+# Current time
 from datetime import datetime
 
 
-def logger():
+# Cleaner function
+def clean_temp_files():
 
-    with open("logs.txt", "a") as file:
-
-        file.write(
-
-            f"{datetime.now()} -> Running\n"
-        )
-
-    print("Log Saved")
+    # Current folder ki files
+    files = os.listdir()
 
 
-schedule.every(5).seconds.do(logger)
+    # Har file check
+    for file in files:
 
 
+        # TMP file check
+        if file.endswith(".tmp"):
+
+
+            # File delete
+            os.remove(file)
+
+
+            print(file, "Deleted")
+
+
+            # Log save
+            with open(
+
+                "cleaner_logs.txt",
+
+                "a"
+            ) as log:
+
+
+                log.write(
+
+                    f"{datetime.now()} -> {file} Deleted\n"
+                )
+
+
+# Every 10 seconds
+schedule.every(10).seconds.do(clean_temp_files)
+
+
+print("Cleaner Running...")
+
+
+# Infinite loop
 while True:
 
+
+    # Scheduled tasks run
     schedule.run_pending()
 
+
+    # CPU rest
     time.sleep(1)
