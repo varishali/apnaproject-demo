3dc7f31f6444bc200221e3dd5b1e878f66efcd63
student apnaproject <varishali886885@gmail.com>
2026-07-01 23:02:18 +0530

    Build OOP employee management system

--- varish.py
+++ varish.py
@@ -1,66 +1,87 @@
-class Bank:
-    def __init__(self, name ,balance):
+class Employee:
+
+    def __init__(self,name,emp_id,salary):
         self.name = name
-        self.balance = balance
+        self.emp_id = emp_id 
+        self.salary = salary
 
-        self.history = []
+    def show_details(self):
+        print("== EMPLOYEE DETAILS ==")
+        print(f"Employee Name : {self.name}")
+        print(f"Emplyee ID : {self.emp_id}")
+        print(f"Employee Salary : {self.salary}")
 
-    def deposit(self):
+class Management:
+    def __init__(self):
+        self.employees = []
 
-        amount = float(input("Enter the amount to deposite : "))
-        self.balance += amount
-        self.history.append(f"Deposited : {amount}")
-        print("Amount deposited successfully.")
+    def add_employee(self):
+        name = input("Enter Employee Name : ")
+        emp_id = input("Enter Employee ID : ")
+        salary = float(input("Enter Employee Salary : "))
+        emp = Employee(name, emp_id, salary)
+        self.employees.append(emp)
+        print("Employee Added Successfully!")
 
-    def withdraw(self):
-        amount = float(input("Enter the amount to withdraw : "))
-        if amount > self.balance:
-            print("Insufficient balance.")
-        else:
-            self.balance -= amount
-            self.history.append(f"withdrawn : {amount}")
-            print("Amount withdrawn successfully.")
-
-    def check_balance(self):
-        print(f"current balance : {self.balance}")
-
-    def transaction_history(self):
-
-        if len(self.history) == 0 :
-            print("No transaction history available.")
+    def show_all_employees(self):
+        if len(self.employees) == 0:
+            print("No Employees Found!")
 
         else:
-            print("Transaction History :")        
+            for emp in self.employees:
+                emp.show_details()
 
-            for item in self.history:
-                print(item)
+    def search_employees(self):
+        search_id = input("Enter Employee ID to search : ")
+        found = False
 
-user1 = Bank("Varish", 10000)
+        for emp in self.employees:
+
+            if emp.emp_id == search_id:
+                emp.show_details()
+
+                found = True
+                
+        if found == False:
+            print("Employee Not Found!")
+
+    def delete_employee(self):
+
+        delete_id = input("Enter Employee ID to delete : ")
+        for emp in self.employees:
+            if emp.emp_id == delete_id:
+                self.employees.remove(emp)
+                print("Employee Delete Successfully!")
+                return
+            
+        print("Employee Not Found! ")
+
+system = Management()
 
 while True:
-    print("\n=====  WELCOME TO THE VR BANK  =====")
-    print("1. Deposit")
-    print("2. Withdraw")
-    print("3. Check Balance")
-    print("4. Transaction History")
+    print("\n=====  EMPLOYEE MANAGEMENT SYSTEM  =====")
+    print("1. Add Employee")
+    print("2. View Employee")
+    print("3. Search Employee")
+    print("4. Delete Employee ")
     print("5. Exit")
 
-    choice = input("Enter your choice (1 - 5) : ")
+    choice = input("Enter Your Choice : ")
+
     if choice == "1":
-        user1.deposit()
+        system.add_employee()
 
     elif choice == "2":
-        user1.withdraw()     
+        system.show_all_employees()
 
     elif choice == "3":
-        user1.check_balance()
+        system.search_employees()
 
     elif choice == "4":
-        user1.transaction_history()
+        system.delete_employee()
 
     elif choice == "5":
-        print("Thanks for using VR Bank . ") 
-        break
-
+        print("Exiting the Program...")
+        break 
     else:
-        print("Invalid Choice. Please try again.")            
+        print("Invalid choice please try again!")                 
