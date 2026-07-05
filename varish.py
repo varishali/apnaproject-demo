deeeb9ee83e69a3a0961a3f3b9313651ce068ce9
student apnaproject <varishali886885@gmail.com>
2026-07-05 15:48:27 +0530

    Scatter Plot Program

--- varish.py
+++ varish.py
@@ -1,80 +1,24 @@
-# Candidates
-votes = {
+import matplotlib.pyplot as plt
 
-    "Ali": 0,
+people = ["P A","P B","P C","P D","P E",
+          "P F","P G","P H","P I","P J"]
 
-    "Varish": 0,
+age = [22,25,30,35,40,45,50,55,60,65]
+bp = [110,115,120,122,125,130,135,123,145,150]
 
-    "Aman": 0
-}
+colors = ["green" if x < 135 else "red" for x in bp]
 
+plt.scatter(age,bp, s=bp, cmap="plasma",c=bp)
+plt.title("Age vs BP")
+plt.xlabel("Age")
+plt.ylabel("Blood Pressure")
 
-# Voted users
-voted_users = []
+plt.grid()
+plt.colorbar(label="BP")
+for i in range(len(people)):
+    plt.annotate(people[i], xy=(age[i], bp[i]), xytext=(age[i]+1,bp[i]+1))
 
+plt.xlim(min(age),max(age)+10)
+plt.ylim(min(bp),max(bp)+5)
 
-while True:
-
-
-    print("\n===== ONLINE VOTING SYSTEM =====")
-
-
-    # User ID
-    user = input("Enter User ID : ")
-
-
-    # Already voted
-    if user in voted_users:
-
-
-        print("You Already Voted ")
-
-
-    else:
-
-
-        print("\nCandidates :")
-
-
-        for candidate in votes:
-
-            print(candidate)
-
-
-        # Vote input
-        vote = input("Enter Candidate Name : ")
-
-
-        # Candidate exists
-        if vote in votes:
-
-
-            votes[vote] += 1
-
-
-            voted_users.append(user)
-
-
-            print("Vote Submitted ")
-
-
-        else:
-
-
-            print("Invalid Candidate")
-
-
-    # Exit
-    stop = input("\nStop Voting ? (yes/no) : ")
-
-
-    if stop == "yes":
-
-        break
-
-print("\n===== FINAL RESULT =====")
-
-
-for candidate, total in votes.items():
-
-    print(candidate, ":", total)
+plt.show()
