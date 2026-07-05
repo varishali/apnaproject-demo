d9962c5031804c6f95183608325d4f27cce7d04c
student apnaproject <varishali886885@gmail.com>
2026-07-05 18:38:42 +0530

    Pie Chart

--- varish.py
+++ varish.py
@@ -1,21 +1,13 @@
 import matplotlib.pyplot as plt
 
-# multiple plots
-cities = ["City A","City B","City C","City D","City E"]
+expenses = ["er","wt","as","ui","op"]
+amounts = [500,150,120,100,50]
 
-# winter : temprature
-winter_temp = [5,2,10,0,7]
-winter_humidity = [80,75,65,85,70]
-
-# summer : temprature
-summer_temp = [25,30,28,35,27]
-summer_humidity = [60,50,55,45,65]
-
-plt.scatter(winter_temp,winter_humidity, label="Winter")
-plt.scatter(summer_temp,summer_humidity, label="Summer")
-
-plt.title("Summer VS Winter Data")
-plt.xlabel("Temprature")
-plt.ylabel("Humidity")
-plt.legend()
+plt.style.use("default")
+explode = [0,0,0,0.2,0]
+plt.pie(amounts,
+        labels=expenses,
+        autopct="%1.1f%%",
+        explode=explode,
+        shadow=True)
 plt.show()
