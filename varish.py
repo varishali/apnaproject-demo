c9cac367752a4a86e15e579bc7fdda93a4b72de6
student apnaproject <varishali886885@gmail.com>
2026-07-05 15:52:06 +0530

    Show Summer VS Winter Data by using matplotlib

--- varish.py
+++ varish.py
@@ -1,24 +1,21 @@
 import matplotlib.pyplot as plt
 
-people = ["P A","P B","P C","P D","P E",
-          "P F","P G","P H","P I","P J"]
+# multiple plots
+cities = ["City A","City B","City C","City D","City E"]
 
-age = [22,25,30,35,40,45,50,55,60,65]
-bp = [110,115,120,122,125,130,135,123,145,150]
+# winter : temprature
+winter_temp = [5,2,10,0,7]
+winter_humidity = [80,75,65,85,70]
 
-colors = ["green" if x < 135 else "red" for x in bp]
+# summer : temprature
+summer_temp = [25,30,28,35,27]
+summer_humidity = [60,50,55,45,65]
 
-plt.scatter(age,bp, s=bp, cmap="plasma",c=bp)
-plt.title("Age vs BP")
-plt.xlabel("Age")
-plt.ylabel("Blood Pressure")
+plt.scatter(winter_temp,winter_humidity, label="Winter")
+plt.scatter(summer_temp,summer_humidity, label="Summer")
 
-plt.grid()
-plt.colorbar(label="BP")
-for i in range(len(people)):
-    plt.annotate(people[i], xy=(age[i], bp[i]), xytext=(age[i]+1,bp[i]+1))
-
-plt.xlim(min(age),max(age)+10)
-plt.ylim(min(bp),max(bp)+5)
-
+plt.title("Summer VS Winter Data")
+plt.xlabel("Temprature")
+plt.ylabel("Humidity")
+plt.legend()
 plt.show()
