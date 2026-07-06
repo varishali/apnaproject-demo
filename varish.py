aa6772c84eddd5b8d82b7eb37fc3f4cf4e66c928
student apnaproject <varishali886885@gmail.com>
2026-07-06 19:03:57 +0530

    Compare Temprature Using subplot and loops

--- varish.py
+++ varish.py
@@ -1,12 +1,26 @@
 import matplotlib.pyplot as plt
-days = ["mon","Tues","Wed","Thur","Fri","Sat","Sun"]
-direct = [50,60,70,80,90,100,110]
-organic = [30,40,50,55,60,70,80]
-social = [20,25,30,35,40,50,60]
 
-plt.stackplot(days,direct,organic,social, labels=["Direect","Organic","Social"])
-plt.title("Marketing data for the week")
-plt.xlabel("Days")
-plt.ylabel("# of customers")
-plt.legend()
+days = ["Mon","Tue","Wed","Thu","Fri"]
+
+cities = ["New York","London","Dehli","Tokyo"]
+
+tempratures = [
+    [22,23,21,24,25],  # New York
+    [18,19,17,20,21],  # london
+    [30,32,31,33,34],  # Dehli
+    [25,26,24,27,28]   # Tokyo
+]
+fig, axes = plt.subplots(2, 2)
+
+city_no = 0
+for i in range(2):
+    for j in range(2):
+        axes[i][j].plot(days, tempratures[city_no],marker='o')
+        axes[i][j].set_title(cities[city_no])
+        axes[i][j].grid(True)
+        city_no = city_no + 1
+fig.suptitle("Tempratures in cities over the week")   
+fig.supylabel("Tempretures")
+fig.supxlabel("Days")
+fig.tight_layout()
 plt.show()
