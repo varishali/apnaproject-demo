37affc4dd3c5b71a45d081c9e01e22c846b0d168
student apnaproject <varishali886885@gmail.com>
2026-07-06 22:44:40 +0530

    Sales_dashboard

--- varish.py
+++ varish.py
@@ -1,26 +1,99 @@
 import matplotlib.pyplot as plt
 
-days = ["Mon","Tue","Wed","Thu","Fri"]
 
-cities = ["New York","London","Dehli","Tokyo"]
+# Months
+months = [
 
-tempratures = [
-    [22,23,21,24,25],  # New York
-    [18,19,17,20,21],  # london
-    [30,32,31,33,34],  # Dehli
-    [25,26,24,27,28]   # Tokyo
+    "Jan",
+
+    "Feb",
+
+    "Mar",
+
+    "Apr",
+
+    "May"
 ]
+
+
+# Product sales
+products = [
+
+    "Laptop",
+
+    "Mobile",
+
+    "Tablet",
+
+    "Camera"
+]
+
+
+sales = [
+
+    [50, 60, 55, 70, 80],   # Laptop
+
+    [40, 45, 50, 55, 60],   # Mobile
+
+    [20, 25, 22, 30, 35],   # Tablet
+
+    [15, 18, 20, 25, 28]    # Camera
+]
+
+
+# Create 2x2 graphs
 fig, axes = plt.subplots(2, 2)
 
-city_no = 0
+
+product_no = 0
+
+
+# Nested loop
 for i in range(2):
+
+
     for j in range(2):
-        axes[i][j].plot(days, tempratures[city_no],marker='o')
-        axes[i][j].set_title(cities[city_no])
+
+
+        # Plot graph
+        axes[i][j].bar(
+
+            months,
+
+            sales[product_no]
+        )
+
+
+        # Graph title
+        axes[i][j].set_title(
+
+            products[product_no]
+        )
+
+
+        # X label
+        axes[i][j].set_xlabel("Months")
+
+
+        # Y label
+        axes[i][j].set_ylabel("Sales")
+
+
+        # Grid
         axes[i][j].grid(True)
-        city_no = city_no + 1
-fig.suptitle("Tempratures in cities over the week")   
-fig.supylabel("Tempretures")
-fig.supxlabel("Days")
+
+
+        # Next product
+        product_no += 1
+
+
+# Main title
+fig.suptitle("Product Sales Dashboard")
+
+
+# Auto spacing
 fig.tight_layout()
+
+
+# Show graph
 plt.show()
