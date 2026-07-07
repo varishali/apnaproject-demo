38d1eee76d7803a78584981d626ac08ce018a625
student apnaproject <varishali886885@gmail.com>
2026-07-07 21:00:40 +0530

    Hospital Dashboard

--- varish.py
+++ varish.py
@@ -1,55 +1,48 @@
 import matplotlib.pyplot as plt
 
-# Months
-months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
+# Departments
+departments = ["ICU", "Emergency", "General", "Pediatrics", "Surgery"]
 
-# Department Profit
-departments = {
-    "IT": [45, 50, 55, 60, 58, 65],
-    "HR": [15, 18, 20, 22, 21, 23],
-    "Sales": [35, 40, 38, 45, 50, 55]
-}
-
-# Total Profit Calculate
-total_profit = []
-
-for i in range(len(months)):
-
-    total = 0
-
-    for dept in departments:
-
-        total += departments[dept][i]
-
-    total_profit.append(total)
+# Data
+patients = [35, 60, 45, 30, 40]
+doctors = [10, 15, 12, 8, 11]
+beds = [40, 70, 50, 35, 45]
 
 # Create Dashboard
 fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 
-# ---------------- IT ----------------
-axes[0][0].plot(months, departments["IT"], marker="o")
-axes[0][0].set_title("IT Department")
+# Patients
+
+axes[0][0].bar(departments, patients)
+axes[0][0].set_title("Patients")
 axes[0][0].grid(True)
 
-# ---------------- HR ----------------
-axes[0][1].bar(months, departments["HR"])
-axes[0][1].set_title("HR Department")
+# Doctors
+
+axes[0][1].plot(departments, doctors, marker="o")
+axes[0][1].set_title("Doctors")
 axes[0][1].grid(True)
 
-# -------------- Sales ---------------
-axes[1][0].plot(months, departments["Sales"], marker="s")
-axes[1][0].set_title("Sales Department")
+# Beds
+
+axes[1][0].scatter(departments, beds)
+axes[1][0].set_title("Available Beds")
 axes[1][0].grid(True)
 
-# -------- Total Profit --------------
-axes[1][1].pie(
-    total_profit,
-    labels=months,
-    autopct="%1.1f%%"
-)
-axes[1][1].set_title("Monthly Profit Share")
+# Occupancy Percentage
 
-fig.suptitle("Company Profit Dashboard")
+occupancy = []
+
+for i in range(len(departments)):
+    percentage = (patients[i] / beds[i]) * 100
+    occupancy.append(percentage)
+
+axes[1][1].plot(departments, occupancy, marker="*", linewidth=2)
+axes[1][1].set_title("Bed Occupancy %")
+axes[1][1].grid(True)
+
+# Main Title
+fig.suptitle("Hospital Management Dashboard")
 
 plt.tight_layout()
 
