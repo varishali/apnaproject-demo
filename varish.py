c94b4628a1167840ab8e528e4528b02def607cc7
student apnaproject <varishali886885@gmail.com>
2026-07-09 22:31:06 +0530

    Random File Name Generator

--- varish.py
+++ varish.py
@@ -1,54 +1,9 @@
-candidates = {
-    "Ali": 0,
-    "Varish": 0,
-    "Aman": 0
-}
+import random
+import string
 
-while True:
+name = ""
 
-    print("\n===== Voting System =====")
+for i in range(8):
+    name += random.choice(string.ascii_lowercase)
 
-    print("1. Vote")
-    print("2. Result")
-    print("3. Exit")
-
-    choice = input("Enter Choice : ")
-
-    if choice == "1":
-
-        print("\nCandidates")
-
-        for name in candidates:
-            print(name)
-
-        vote = input("Vote For : ")
-
-        if vote in candidates:
-            candidates[vote] += 1
-            print("Vote Recorded")
-        else:
-            print("Invalid Candidate")
-
-    elif choice == "2":
-
-        print("\n===== Result =====")
-
-        winner = ""
-        max_vote = -1
-
-        for name, vote in candidates.items():
-
-            print(name, ":", vote)
-
-            if vote > max_vote:
-                max_vote = vote
-                winner = name
-
-        print("\nWinner :", winner)
-
-    elif choice == "3":
-        print("Voting Closed")
-        break
-
-    else:
-        print("Invalid Choice")
+print(name + ".txt")
