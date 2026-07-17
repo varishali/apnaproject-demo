bd89628c716b98aa331d8ffe2e248dd74a8def07
student apnaproject <varishali886885@gmail.com>
2026-07-17 20:38:25 +0530

    Pandas csv file read program

--- varish.py
+++ varish.py
@@ -1,42 +1,23 @@
 import pandas as pd
 
-# read CSV file
+# read csv
 df = pd.read_csv("file.csv")
 
-print("\n\033[1;103m==========================  STUDENTS RESULT ANALYZER  ==========================\033[0m\n")
+print("\n----------------- Employee Salary Analyzer ------------------\n")
 
-print("\n\033[1;102m--------------   STUDENT DATA  ------------------\033[0m\n")
-print("\033[0;91m",df,"\033[0m")
-
-
-# total marks
-df["Total"] = df["Math"] + df["Science"] + df["English"]
-
-# average marks
-df["Average"] = df["Total"] / 3
+# show data
+print("---------------------- Employee Data ------------------------")
+print(df)
 
 
-# result
-df["Result"] = df["Average"].apply(lambda x: "Pass" if x>=40 else "Fail")
+# highest salary
+print("\nHighest Salary Employee : ")
+print(df.loc[df["Salary"].idxmax()])
 
+# lowest salary
+print("\nLowest Salary Employee : ")
+print(df.loc[df["Salary"].idxmin()])
 
-# grade
+# avarage salary
+print("\nAverage Salary : ",round(df["Salary"].mean(),2))
 
-def grade(avg):
-    if avg >= 90:
-        return "A+"
-    elif avg >= 80:
-        return "A"
-    elif avg >= 70:
-        return "B"
-    elif avg >= 60:
-        return "C"
-    else:
-        return "D"
-
-df["Grade"] = df["Average"].apply(grade)
-
-
-# show update data 
-print("\n\033[1;102m----------------------------------- RESULT ------------------------------------\033[0m\n")
-print("\033[0;91m",df,"\033[0m")    
