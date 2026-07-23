218e8523c409a70b224e7c01d6ff3bc7006698a5
student apnaproject <varishali886885@gmail.com>
2026-07-23 23:00:35 +0530

    Personal Expense Tracker

--- varish.py
+++ varish.py
@@ -1,53 +1,59 @@
 import pandas as pd
 import os
 
-FILE = "students.csv"
+FILE = "expenses.csv"
 
-# Create CSV if not exists
+# Create file if not exists
 if not os.path.exists(FILE):
-    df = pd.DataFrame({
-        "ID": [1, 2, 3, 4, 5],
-        "Name": ["Aman", "Rahul", "Priya", "Neha", "Rohan"],
-        "Math": [85, 72, 91, 68, 77],
-        "Science": [80, 75, 95, 70, 82],
-        "English": [78, 88, 90, 72, 79]
-    })
+    df = pd.DataFrame(columns=["Date", "Category", "Amount"])
     df.to_csv(FILE, index=False)
 
-# Read CSV
-df = pd.read_csv(FILE)
+while True:
+    print("\n====== Expense Tracker ======")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Total Expense")
+    print("4. Exit")
 
-# Total & Average
-df["Total"] = df["Math"] + df["Science"] + df["English"]
-df["Average"] = (df["Total"] / 3).round(2)
+    choice = input("Enter Choice: ")
 
-# Grade
-def grade(avg):
-    if avg >= 90:
-        return "A+"
-    elif avg >= 80:
-        return "A"
-    elif avg >= 70:
-        return "B"
-    elif avg >= 60:
-        return "C"
+    if choice == "1":
+        date = input("Date (DD-MM-YYYY): ")
+        category = input("Category: ")
+        amount = float(input("Amount: "))
+
+        df = pd.read_csv(FILE)
+
+        new = pd.DataFrame({
+            "Date": [date],
+            "Category": [category],
+            "Amount": [amount]
+        })
+
+        df = pd.concat([df, new], ignore_index=True)
+        df.to_csv(FILE, index=False)
+
+        print("Expense Added Successfully!")
+
+    elif choice == "2":
+        df = pd.read_csv(FILE)
+
+        if df.empty:
+            print("No Expenses Found!")
+        else:
+            print(df)
+
+    elif choice == "3":
+        df = pd.read_csv(FILE)
+
+        print("\nTotal Expense:", df["Amount"].sum())
+
+        print("\nCategory Wise Expense")
+        print(df.groupby("Category")["Amount"].sum())
+
+    elif choice == "4":
+        print("Thank You!")
+        break
+
     else:
-        return "Fail"
-
-df["Grade"] = df["Average"].apply(grade)
-
-print("\n===== STUDENT REPORT =====")
-print(df)
-
-print("\nTop Student:")
-print(df.loc[df["Total"].idxmax()])
-
-print("\nClass Statistics")
-print("Highest Total :", df["Total"].max())
-print("Lowest Total  :", df["Total"].min())
-print("Average Marks :", round(df["Average"].mean(), 2))
-
-# Save result
-df.to_csv("student_result.csv", index=False)
-
-print("\nResult saved as 'student_result.csv'")
+        print("Invalid Choice!")
