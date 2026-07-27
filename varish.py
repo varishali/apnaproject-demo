43ce5012d543f5226d28608e43cb30b9a6ae2d56
student apnaproject <varishali886885@gmail.com>
2026-07-27 19:11:20 +0530

    Expense-Tracker-Python

--- varish.py
+++ varish.py
@@ -1,49 +1,42 @@
 import csv
 import os
 
-FILE = "students.csv"
+FILE = "expenses.csv"
 
 if not os.path.exists(FILE):
     with open(FILE, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["Name", "Math", "Science", "English", "Total", "Percent", "Grade"])
+        writer.writerow(["Category", "Amount"])
 
-def grade(percent):
-    if percent >= 90:
-        return "A+"
-    elif percent >= 75:
-        return "A"
-    elif percent >= 60:
-        return "B"
-    elif percent >= 45:
-        return "C"
-    else:
-        return "Fail"
-
-def add_student():
-    name = input("Student Name: ")
-    math = int(input("Math Marks: "))
-    science = int(input("Science Marks: "))
-    english = int(input("English Marks: "))
-
-    total = math + science + english
-    percent = round(total / 3, 2)
-    g = grade(percent)
+def add_expense():
+    category = input("Category: ")
+    amount = float(input("Amount: "))
 
     with open(FILE, "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow([name, math, science, english, total, percent, g])
+        writer.writerow([category, amount])
 
-    print("Student Record Saved!")
+    print("Expense Added Successfully!")
 
-def show_records():
+def view_expenses():
+    total = 0
+
     with open(FILE, "r") as f:
         reader = csv.reader(f)
-        for row in reader:
-            print(row)
+        next(reader)
 
-def search_student():
-    name = input("Enter Student Name: ")
+        print("\nCategory\tAmount")
+
+        for row in reader:
+            print(f"{row[0]}\t\t₹{row[1]}")
+            total += float(row[1])
+
+    print("-" * 30)
+    print("Total Expense = ₹", total)
+
+def search_category():
+    category = input("Enter Category: ")
+
     found = False
 
     with open(FILE, "r") as f:
@@ -51,39 +44,33 @@ def search_student():
         next(reader)
 
         for row in reader:
-            if row[0].lower() == name.lower():
-                print("\nName      :", row[0])
-                print("Math      :", row[1])
-                print("Science   :", row[2])
-                print("English   :", row[3])
-                print("Total     :", row[4])
-                print("Percent   :", row[5])
-                print("Grade     :", row[6])
+            if row[0].lower() == category.lower():
+                print(row)
                 found = True
 
     if not found:
-        print("Student Not Found!")
+        print("No Record Found!")
 
 while True:
-    print("\n===== Student Report Card Manager =====")
-    print("1. Add Student")
-    print("2. Show All Records")
-    print("3. Search Student")
+    print("\n===== Expense Tracker =====")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Search Category")
     print("4. Exit")
 
     choice = input("Enter Choice: ")
 
     if choice == "1":
-        add_student()
+        add_expense()
 
     elif choice == "2":
-        show_records()
+        view_expenses()
 
     elif choice == "3":
-        search_student()
+        search_category()
 
     elif choice == "4":
-        print("Thank You!")
+        print("Good Bye!")
         break
 
     else:
