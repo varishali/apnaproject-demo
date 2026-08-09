94fa3ff8b067868f04ca17da6ddb3f072dfeb130
student apnaproject <varishali886885@gmail.com>
2026-08-09 22:58:00 +0530

    Rolling a Dice

--- varish.py
+++ varish.py
@@ -1,69 +1,11 @@
 import random
-import string
-
-def generate_password(length):
-    characters = (
-        string.ascii_letters +
-        string.digits +
-        string.punctuation
-        )
-
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
-def password_strength(password):
-    score = 0
-    if any(c.islower() for c in password):
-        score += 1
-
-    if any(c.isupper() for c in password):
-        score += 1
-
-    if any(c.isdigit() for c in password):
-        score += 1
-
-    if any(c in string.punctuation for c in password):
-        score += 1
-
-    if len(password) >= 12:
-        score += 1
-
-    if score <= 2:
-        return 'Weak'
-
-    elif score == 3 or score == 4:
-        return 'Medium'
-
-    else:
-        return 'Strong'
-def main():
-    print('=== Password Generator CLI ===')
-
 
 while True:
-    try:
-        length = int(input('Enter password length (6-32): '))
-
-        if 6 <= length <= 32:
-            password = generate_password(length)
-
-            print('\\nGenerated Password:')
-            print(password)
-
-            print('\\nStrength:')
-            print(password_strength(password))
-
-            again = input('\\nGenerate another password? (y/n): ').lower()
-
-            if again != 'y':
-                print('Goodbye!')
-                break
-
-        else:
-            print('Please enter a number between 6 and 32.')
-
-    except ValueError:
-        print('Please enter a valid number.')
-
-
-if __name__ == '__main__':
-    main()
+    input("Press Enter To Roll Dice...")
+    roll = random.randint(1, 6)
+    print(f"You Rolled : {roll}")
+    
+    again = input("Roll Again? (y/n) : ")
+    if again.lower() != 'y':
+        print("Game Over!")
+        break
