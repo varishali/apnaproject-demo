62b78637e180e0654c265d314a95762ee2b76c1b
student apnaproject <varishali886885@gmail.com>
2026-08-27 14:40:23 +0530

    Add Turtle program to write text on screen

--- varish.py
+++ varish.py
@@ -1,17 +1,10 @@
 import turtle
 
-def draw_tree(branch_len, t):
-    if branch_len > 5:
-        t.forward(branch_len)
-        t.right(20)
-        draw_tree(branch_len - 15, t)
-        t.left(40)
-        draw_tree(branch_len - 15, t)
-        t.right(20)
-        t.backward(branch_len)
-
 t = turtle.Turtle()
-t.left(90)
-t.color('green')
-draw_tree(100, t)
-turtle.done()        
+
+t.penup()
+t.goto(-100, 0)
+t.pendown()
+t.color('red')
+t.write('VARISH ALI', font=('Arial', 50, 'bold'))
+turtle.done()
